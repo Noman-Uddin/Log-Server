35b832e7f9a1de29e12fd92ff0be101002d1f50b
Noman-Uddin <94782747+Noman-Uddin@users.noreply.github.com>
2023-05-17 12:16:16 +0600

    Update auto_install.sh

--- LOG/auto_install.sh
+++ LOG/auto_install.sh
@@ -161,5 +161,19 @@ echo "Installation Done"
 
 
 #Self Delete
-
 rm -f auto_install.sh
+
+# Installing Zabbix Agent
+
+wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-4+ubuntu18.04_all.deb
+
+sudo dpkg -i zabbix-release_6.0-4+ubuntu18.04_all.deb
+
+sudo apt update
+
+sudo apt install zabbix-agent
+
+
+
+
+
